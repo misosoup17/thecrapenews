0fd4bec6c170939f9aa889d424a31ec2bb922046
misosoup17 <misagaspar@icloud.com>
2020-01-20 01:58:40 -0800

    updated server

--- server.js
+++ server.js
@@ -1,47 +1,95 @@
-//Dependencies
-const express = require("express");
-const exphbs = require("express-handlebars");
-const mongoose = require("mongoose");
-const logger = require("morgan");
-// Scraping tools
-const axios = require("axios");
-const cheerio = require("cheerio");
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-//Require all models
+// Require all models
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
 
-//Initialize express
+// Initialize Express
 var app = express();
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scraper", {
-  useNewUrlParser: true
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapenews";
+
+mongoose.connect(MONGODB_URI);
+
+// Routes
+
+
+  // First, we grab the body of the html with axios
+  axios.get("http://www.news.google.com").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+    $("article").each(function (i, element) {
+      var result = {};
+
+      result.title = $(this)
+        .children("h4")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+  });
+
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+  db.article.find()
+    .then(function (dbPopulate) {
+      res.json(dbPopulate);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
-//Necc. for heroku to read public folder
-app.use(express.static(__dirname + "/public"));
-
-// handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-//routes
-require("./config/routes");
-
-//start server
-app.listen(PORT, function() {
-  console.log("http://localhost:" + PORT);
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function (req, res) {
+  db.article.findById(req.params.id)
+    .populate("note")
+    .then(function (dbPopulate) {
+      res.json(dbPopulate);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
-module.exports = app;
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+  db.Note.create(req.body)
+    .then(function (dbPopulate) {
+
+      return db.article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbPopulate._id } }, { new: true });
+    })
+    .then(function (dbPopulate) {
+      res.json(dbPopulate);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
